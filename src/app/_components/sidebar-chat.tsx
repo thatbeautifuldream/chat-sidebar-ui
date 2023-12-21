810c0a9e44806fef09610affaf1d6d90af84a550
Milind Mishra ▲ <milind.mishra4@gmail.com>
2023-12-21 21:59:35 +0530

    📦 NEW: add chat input

--- src/app/_components/sidebar-chat.tsx
+++ src/app/_components/sidebar-chat.tsx
@@ -4,6 +4,7 @@ import { Fragment, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { XMarkIcon } from "@heroicons/react/24/outline";
 import ChatCard from "@/app/_components/chat-card";
+import ChatInput from "@/app/_components/chat-input";
 
 export default function SidebarChat() {
   const [open, setOpen] = useState(true);
@@ -61,20 +62,8 @@ export default function SidebarChat() {
                           <ChatCard />
                         </div>
                       </div>
-                      <div className="flex flex-shrink-0 justify-end px-4 py-4">
-                        <button
-                          type="button"
-                          className="rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400"
-                          onClick={() => setOpen(false)}
-                        >
-                          Cancel
-                        </button>
-                        <button
-                          type="submit"
-                          className="ml-4 inline-flex justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                        >
-                          Send
-                        </button>
+                      <div className="px-4 py-4">
+                        <ChatInput />
                       </div>
                     </div>
                   </Dialog.Panel>
